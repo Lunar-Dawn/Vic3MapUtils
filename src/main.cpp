aaf342e58540d420e23cd540e23dd492501fb92c
Lunar-Dawn <lunarrequiem42@gmail.com>
2024-11-07 15:38:47 +0100

    Refactor command line interface to align better with standard usage

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <filesystem>
 #include <fstream>
 #include <iostream>
+#include <string_view>
+#include <vector>
 
 #include <nlohmann/json.hpp>
 
@@ -10,82 +12,134 @@
 namespace fs = std::filesystem;
 using json = nlohmann::json;
 
-void printUsage(std::string_view fileName) {
-	std::cout << "Usage: " << fs::path(fileName).filename().string()
-	          << " <generate | apply> [args]\n"
-	             "\n"
-	             "A utility to transfer your spline network edits across Victoria 3 versions.\n"
-	             "\n"
-	             "Commands:\n"
-	             "    generate originalNetworkFile editedNetworkFile [output]\n"
-	             "        Generates a network diff file from the original (usually vanilla's) "
-	             "and the edited (usually your mod's) splnet files.\n"
-	             "        The diff will be saved as diff.json unless output is specified.\n"
+using ArgList = std::vector<std::string_view>;
 
-	             "    apply networkFile diff [output]\n"
-	             "        Applies the network diff to the splnet file (usually vanilla's).\n"
-	             "        The edited network will override the original unless output is specified.\n";
+// TODO: Revisit this, either using an argument parsing framework or making each command a class
+void printUsage(const ArgList &arguments) {
+	std::cout
+	    << "Usage: " << fs::path(arguments[0]).filename().string()
+	    << " <command> [options] <args>\n"
+	       "\n"
+	       "A utility to transfer your spline network edits across Victoria 3 versions.\n"
+	       "\n"
+	       "General Options:\n"
+	       "    -o <file>, --output <file>: Sets the output file name. Optional, default behaviour varies by command.\n"
+	       "Commands:\n"
+	       "    help\n"
+	       "        Print this help information\n"
+	       "    generate [-o <file>] originalNetworkFile editedNetworkFile\n"
+	       "        Generates a network diff file from the original (usually vanilla's) "
+	       "and the edited (usually your mod's) splnet files.\n"
+	       "        The diff will be saved as diff.json unless output is specified.\n"
+	       "    apply [-o <file>] networkFile diff\n"
+	       "        Applies the network diff to the splnet file (usually vanilla's).\n"
+	       "        The edited network will override the original unless output is specified.\n";
 }
 
-void handleGenerate(int argc, char *argv[]) {
-	if (argc < 4 || argc > 6) {
-		printUsage(argv[0]);
+void handleGenerate(const ArgList &arguments) {
+	if (arguments.size() < 4 || arguments.size() > 6) {
+		printUsage(arguments);
 		std::exit(1);
 	}
 
-	fs::path originalNetworkFile(argv[2]);
-	fs::path editedNetworkFile(argv[3]);
+	fs::path originalNetworkPath;
+	fs::path editedNetworkPath;
+	fs::path outputPath;
 
-	if (!checkFilesExist(originalNetworkFile, editedNetworkFile)) {
+	auto it = arguments.begin() + 2;
+
+	if (*it == "-o" || *it == "--output") {
+		it++;
+		outputPath = *it++;
+	} else {
+		outputPath = "diff.json";
+	}
+
+	if (it == arguments.end()) {
+		printUsage(arguments);
 		std::exit(1);
-	};
+	}
+	originalNetworkPath = *it++;
 
-	SplineNetwork originalNetwork(originalNetworkFile);
-	SplineNetwork editedNetwork(editedNetworkFile);
+	if (it == arguments.end()) {
+		printUsage(arguments);
+		std::exit(1);
+	}
+	editedNetworkPath = *it++;
+
+	if (!checkFilesExist(originalNetworkPath, editedNetworkPath)) {
+		std::exit(1);
+	}
+
+	SplineNetwork originalNetwork(originalNetworkPath);
+	SplineNetwork editedNetwork(editedNetworkPath);
 
 	json jsonFile = originalNetwork.calculateDiff(editedNetwork);
-	std::ofstream outputFile(argc == 5 ? argv[4] : "diff.json");
+	std::ofstream outputFile(outputPath);
 	outputFile << jsonFile.dump(4) << '\n';
 }
-void handleApply(int argc, char *argv[]) {
-	if (argc < 4 || argc > 6) {
-		printUsage(argv[0]);
+void handleApply(const ArgList &arguments) {
+	if (arguments.size() < 4 || arguments.size() > 6) {
+		printUsage(arguments);
 		std::exit(1);
 	}
 
-	fs::path networkFile(argv[2]);
-	fs::path diffFile(argv[3]);
+	fs::path networkPath;
+	fs::path diffPath;
+	fs::path outputPath;
 
-	if (!checkFilesExist(networkFile, diffFile)) {
+	auto it = arguments.begin() + 2;
+
+	if (*it == "-o" || *it == "--output") {
+		it++;
+		outputPath = *it++;
+	} else {
+		outputPath = "diff.json";
+	}
+
+	if (it == arguments.end()) {
+		printUsage(arguments);
+		std::exit(1);
+	}
+	networkPath = *it++;
+
+	if (it == arguments.end()) {
+		printUsage(arguments);
+		std::exit(1);
+	}
+	diffPath = *it++;
+
+	if (!checkFilesExist(networkPath, diffPath)) {
 		std::exit(1);
 	};
 
-	SplineNetwork network(networkFile);
-	network.applyDiff(json::parse(std::ifstream(diffFile)).get<SplineNetwork::Diff>());
-
-	network.writeToFile(argc == 5 ? argv[4] : argv[2]);
+	SplineNetwork network(networkPath);
+	network.applyDiff(json::parse(std::ifstream(diffPath)).get<SplineNetwork::Diff>());
+	network.writeToFile(outputPath);
 }
 
 int main(int argc, char *argv[]) {
-	if (argc == 1) {
-		printUsage(argv[0]);
+	ArgList arguments(argv, argv + argc);
+
+	if (arguments.size() == 1) {
+		printUsage(arguments);
 		return 1;
 	}
-	std::string command(argv[1]);
 
+	const auto &command = arguments[1];
 	if (command == "help" || command == "-h" || command == "--help") {
-		printUsage(argv[0]);
+		printUsage(arguments);
 		return 0;
 	}
 	if (command == "generate") {
-		handleGenerate(argc, argv);
+		handleGenerate(arguments);
 		return 0;
 	}
 	if (command == "apply") {
-		handleApply(argc, argv);
+		handleApply(arguments);
 		return 0;
 	}
 
-	printUsage(argv[0]);
+	printUsage(arguments);
 	return 1;
 }
